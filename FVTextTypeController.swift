5256a6ee2d254611230737d7ad538cc41747e2ec
Kevin Wojniak <kainjow@kainjow.com>
2015-05-10 17:08:12 -0700

    Add viewer for 'RTF '

--- FVTextTypeController.swift
+++ FVTextTypeController.swift
@@ -10,11 +10,11 @@ import Cocoa
 
 final class FVTextTypeController: FVTypeController {
     func supportedTypes() -> [String] {
-        return ["plst", "TEXT", "utf8", "utxt", "ut16", "weba"]
+        return ["plst", "TEXT", "utf8", "utxt", "ut16", "weba", "RTF "]
     }
     
     func viewControllerFromResource(resource: FVResource, inout errmsg: String) -> NSViewController? {
-        let str = stringFromResource(resource)
+        let str = attributedStringFromResource(resource)
         if str == nil {
             return nil
         }
@@ -24,32 +24,45 @@ final class FVTextTypeController: FVTypeController {
         }
         let scrollView = viewController!.view as! NSScrollView
         let textView = scrollView.documentView as! NSTextView
-        textView.string = str!
+        textView.textStorage?.setAttributedString(str!)
         return viewController
     }
     
-    func stringFromResource(resource: FVResource) -> String? {
+    func attributedStringFromResource(resource: FVResource) -> NSAttributedString? {
         let rsrcData = resource.data
         if rsrcData == nil {
             return nil
         }
         let type = resource.type?.typeString
         switch type! {
+        case "RTF ":
+            return NSAttributedString(RTF: rsrcData!, documentAttributes: nil)
+        default:
+            if let str = stringFromResource(rsrcData!, type: type!) {
+                return NSAttributedString(string: str)
+            }
+            break;
+        }
+        return nil
+    }
+    
+    func stringFromResource(rsrcData: NSData, type: String) -> String? {
+        switch type {
         case "plst", "weba":
-            let plist: AnyObject? = NSPropertyListSerialization.propertyListWithData(rsrcData!, options: NSPropertyListReadOptions(NSPropertyListMutabilityOptions.Immutable.rawValue), format: nil, error: nil)
+            let plist: AnyObject? = NSPropertyListSerialization.propertyListWithData(rsrcData, options: NSPropertyListReadOptions(NSPropertyListMutabilityOptions.Immutable.rawValue), format: nil, error: nil)
             if plist != nil {
                 if let data = NSPropertyListSerialization.dataWithPropertyList(plist!, format: .XMLFormat_v1_0, options: NSPropertyListWriteOptions(0), error: nil) {
                     return NSString(data: data, encoding: NSUTF8StringEncoding) as? String
                 }
             }
         case "TEXT":
-            return NSString(data: rsrcData!, encoding: NSMacOSRomanStringEncoding) as? String
+            return NSString(data: rsrcData, encoding: NSMacOSRomanStringEncoding) as? String
         case "utf8":
-            return NSString(data: rsrcData!, encoding: NSUTF8StringEncoding) as? String
+            return NSString(data: rsrcData, encoding: NSUTF8StringEncoding) as? String
         case "utxt":
-            return NSString(data: rsrcData!, encoding: NSUTF16BigEndianStringEncoding) as? String
+            return NSString(data: rsrcData, encoding: NSUTF16BigEndianStringEncoding) as? String
         case "ut16":
-            return NSString(data: rsrcData!, encoding: NSUnicodeStringEncoding) as? String
+            return NSString(data: rsrcData, encoding: NSUnicodeStringEncoding) as? String
         default:
             break;
         }
