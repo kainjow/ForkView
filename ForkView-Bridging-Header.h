617120be24d3480cdfbe281811365435380608ea
C.W. Betts <computers57@hotmail.com>
2015-05-12 14:30:28 -0600

    Port FVResourceFile to Swift.

--- ForkView-Bridging-Header.h
+++ ForkView-Bridging-Header.h
@@ -2,7 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "FVResourceFile.h"
-#import "FVResourceFilePriv.h"
 #include <sys/xattr.h>
 #import "PICTConverterProtocol.h"
