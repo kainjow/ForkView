3bab81598bb17b6584dfd7c5268ff9435a7ba23e
Kevin Wojniak <kainjow@kainjow.com>
2015-05-12 07:44:56 -0700

    Replace tabs with spaces

--- ExportPalettes/ExportPalettes.c
+++ ExportPalettes/ExportPalettes.c
@@ -16,37 +16,37 @@ int main (int argc, const char * argv[]) {
     extern CTabHandle GetCTable(short);
     extern void DisposeCTable(CTabHandle);
     
-	FILE *f = fopen("palettes.htm", "w");
+    FILE *f = fopen("palettes.htm", "w");
     fprintf(f, "<html><head><title>GetCTable</title><style>td {width: 50px; height: 50px; }</style></head><body>\n");
     short i = -32768;
-	for (int n = 0; n < 65536; ++i, ++n) {
-		CTabHandle t = GetCTable(i);
-		if (t) {
-			CTabPtr ptr = *t;
+    for (int n = 0; n < 65536; ++i, ++n) {
+        CTabHandle t = GetCTable(i);
+        if (t) {
+            CTabPtr ptr = *t;
             const int sz = ptr->ctSize + 1;
             printf("let palette%d: [FVRGBColor] = [\n", i);
-			fprintf(f, "<h1>[%d] %d</h1>\n", i, sz);
-			fprintf(f, "<table>\n");
-			for (int j = 0; j < sz; ++j) {
-				const ColorSpec *s = &ptr->ctTable[j];
-				if (j == 0 || (j % 16) == 0) {
-					if (n > 0) {
-						fprintf(f, "</tr>\n");
-					}
-					fprintf(f, "<tr>\n");
-				}
+            fprintf(f, "<h1>[%d] %d</h1>\n", i, sz);
+            fprintf(f, "<table>\n");
+            for (int j = 0; j < sz; ++j) {
+                const ColorSpec *s = &ptr->ctTable[j];
+                if (j == 0 || (j % 16) == 0) {
+                    if (n > 0) {
+                        fprintf(f, "</tr>\n");
+                    }
+                    fprintf(f, "<tr>\n");
+                }
                 int r = (s->rgb.red / 65535.) * 255;
                 int g = (s->rgb.green / 65535.) * 255;
                 int b = (s->rgb.blue / 65535.) * 255;
                 printf("    FVRGBColor(r: %d, g: %d, b: %d),\n", r, g, b);
                 fprintf(f, "<td bgcolor=\"#%02X%02X%02X\"></td>\n", r, g, b);
-			}
+            }
             printf("]\n");
-			fprintf(f, "</tr></table>\n");
-			DisposeCTable(t);
-		}
+            fprintf(f, "</tr></table>\n");
+            DisposeCTable(t);
+        }
     }
     fprintf(f, "</body></html>\n");
-	fclose(f);
+    fclose(f);
     return 0;
 }
