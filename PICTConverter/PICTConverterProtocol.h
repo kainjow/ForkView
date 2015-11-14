6953100d16d6b17c8879b17fe3103c6ff0189d02
Kevin Wojniak <kainjow@kainjow.com>
2015-11-14 13:31:44 -0800

    Remove comments

--- PICTConverter/PICTConverterProtocol.h
+++ PICTConverter/PICTConverterProtocol.h
@@ -8,29 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-// The protocol that this service will vend as its API. This header file will also need to be visible to the process hosting the service.
 @protocol PICTConverterProtocol
 
-// Replace the API of this protocol with an API appropriate to the service you are vending.
 - (void)convertPICTDataToTIFF:(nonnull NSData *)pictData withReply:(nonnull void (^)(NSData * __nullable))reply;
     
 @end
-
-/*
- To use the service from an application or other process, use NSXPCConnection to establish a connection to the service by doing something like this:
-
-     _connectionToService = [[NSXPCConnection alloc] initWithServiceName:@"com.github.maddthesane.pictConverter2"];
-     _connectionToService.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(StringModifing)];
-     [_connectionToService resume];
-
-Once you have a connection to the service, you can use it like this:
-
-     [[_connectionToService remoteObjectProxy] upperCaseString:@"hello" withReply:^(NSString *aString) {
-         // We have received a response. Update our text field, but do it on the main thread.
-         NSLog(@"Result string was: %@", aString);
-     }];
-
- And, when you are finished with the service, clean up the connection like this:
-
-     [_connectionToService invalidate];
-*/
--- PICTConverter/main.m
+++ PICTConverter/main.m
@@ -26,19 +26,9 @@
 }
 
 - (BOOL)listener:(NSXPCListener *)listener shouldAcceptNewConnection:(NSXPCConnection *)newConnection {
-    // This method is where the NSXPCListener configures, accepts, and resumes a new incoming NSXPCConnection.
-    
-    // Configure the connection.
-    // First, set the interface that the exported object implements.
     newConnection.exportedInterface = [NSXPCInterface interfaceWithProtocol:@protocol(PICTConverterProtocol)];
-    
-    // Next, set the object that the connection exports. All messages sent on the connection to this service will be sent to the exported object to handle. The connection retains the exported object.
     newConnection.exportedObject = converter_;
-    
-    // Resuming the connection allows the system to deliver more incoming messages.
     [newConnection resume];
-    
-    // Returning YES from this method tells the system that you have accepted this connection. If you want to reject the connection for some reason, call -invalidate on the connection and return NO.
     return YES;
 }
 
